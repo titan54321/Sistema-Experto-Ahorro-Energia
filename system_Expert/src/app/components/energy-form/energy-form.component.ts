2cbedfa268eff5d903473697cf0731a13224bf27
Juan <juan_ls58@hotmail.com>
2025-11-26 23:02:19 -0600

    Ng-Zorro implemented

---
+++ system_Expert/src/app/components/energy-form/energy-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-energy-form',
+  imports: [],
+  templateUrl: './energy-form.component.html',
+  styleUrl: './energy-form.component.css'
+})
+export class EnergyFormComponent {
+
+}
